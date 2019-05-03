8d61f6e8722f765a0f9feeb0c899ae40f8d45316
Roberto <00162317@uca.edu.sv>
2019-05-03 13:45:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/examenlabo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.examenlabo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //YA MORI
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
